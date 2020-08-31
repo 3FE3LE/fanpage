23ae134103d27eb6ac243cf06b1ffa7536c3e88b
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2020-08-31 13:11:18 -0500

    add fonts

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ const Home = () => (
     <div className='w-full bg-gray-200 min-h-screen flex flex-col md:flex-row lg:w-10/12 justify-center items-center'>
       <div className="w-full md:w-10/12 bg-gray-300 min-h-screen flex grid grid-rows-5">
         <div className="w-full h-full row-span-3 flex">
-          <div className="w-0 sm:w-2/12 border bg-gray-400 h-full"></div>
+          <div className="w-0 hidden sm:w-2/12 border bg-gray-400 h-full"></div>
           <div className="w-1/2 sm:w-5/12 bg-gray-400 h-full grid grid-rows-3">
             <div className="w-full h-full border bg-gray-500 p-2 row-span-2"></div>
             <div className="w-full h-full border bg-gray-600 p-2 row-span-1"></div>
@@ -20,8 +20,8 @@ const Home = () => (
           <div className="w-1/2 sm:w-5/12 bg-gray-400 border min-h-full"></div>
         </div>
         <div className="w-full h-full row-span-2 flex">
-          <div className="w-2/12 sm:w-4/12 bg-gray-400 border min-h-full"></div>
-          <div className="w-8/12 sm:w-6/12 bg-gray-400 border min-h-full">
+          <div className="w-2/12 sm:w-4/12 bg-white min-h-full"></div>
+          <div className="w-8/12 sm:w-6/12 bg-black min-h-full">
             <div className="flex justify-between w-full h-0 -mt-6 -ml-6">
               <div className="w-0 h-0 relative">
                 <div className="h-12 w-12 text-white font-black bg-gray-400 shadow-md rounded-full flex items-center justify-center">ico</div>
@@ -40,12 +40,12 @@ const Home = () => (
               </div>
             </div>
           </div>
-          <div className="w-2/12 bg-gray-400 border min-h-full p-2"></div>
+          <div className="w-2/12 bg-white min-h-full p-2"></div>
         </div>
       </div>
       <div className="w-full  md:w-2/12 bg-gray-400 min-h-screen grid grid-rows-5 ">
-        <div className="w-full bg-gray-400 border row-span-4 md:row-span-2"></div>
-        <div className="w-full bg-gray-400 border row-span-1 md:row-span-3"></div>
+        <div className="w-full bg-white row-span-4 md:row-span-2"></div>
+        <div className="w-full bg-black   row-span-1 md:row-span-3"></div>
       </div>
     </div>
   </>
