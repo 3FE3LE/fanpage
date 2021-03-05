f6c86d0274e377d7957d4bacf5d80a193abb43c2
Frank Licona (3FE3LE) <3l33f3@gmail.com>
2021-03-05 09:19:27 -0500

    initial changes for rework

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Head from 'next/head'
-import PrincipalSection from '../components/PrincipalSection'
-import SecondSection from '../components/SecondSection'
+import Head from "next/head";
 
 const Home = () => (
-
   <>
     <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lobster&family=Montserrat:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=MuseoModerno:wght@700&display=swap"
+        rel="stylesheet"
+      />
       <title>3FE 3LE</title>
-      <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Montserrat:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=MuseoModerno:wght@700&display=swap" rel="stylesheet" />
-      <script src="https://apis.google.com/js/platform.js"></script>
     </Head>
-    <PrincipalSection/>
+    <h1>I'm 3FE3LE</h1>
   </>
-)
-export default Home
+);
+export default Home;
