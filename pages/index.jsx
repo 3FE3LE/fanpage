e67a30dad145e63818647c2dd3db9a87f2391f66
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2021-04-09 12:03:52 -0500

    so optimized for SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useState } from "react";
 
 export default function Home() {
-  const initialContent = { title: "", content: "", expand: false, side: 'r' };
+  const initialContent = { title: "", content: "", expand: false, side: "r" };
 
   const [content, setContent] = useState(initialContent);
 
@@ -39,12 +39,20 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Frank Licona React Frontend Developer Thanks to software development I found more than a hobby I found my passion.
+          if u want know more about me watch my Youtube Chanel 3FE3LE"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lobster&family=Montserrat:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=MuseoModerno:wght@700&display=swap"
           rel="stylesheet"
         />
-        <title>3FE 3LE</title>
+        <title>3FE 3LE - byFrankLicona</title>
       </Head>
+      <h1 style={{ display: "none" }}>byfranklicona</h1>
       <img
         onClick={() => setContent(initialContent)}
         className="content__button content__button--logo"
@@ -72,11 +80,13 @@ export default function Home() {
       <div className="content">
         <div
           className={`content__info--${
-            content.expand && content.side == 'r' ? "with-content" : "without-content"
+            content.expand && content.side == "r"
+              ? "with-content"
+              : "without-content"
           }`}
         >
-          <h1>{content.side=="r"?content.title:""}</h1>
-          <div>{content.side=="r"?content.content:""}</div>
+          <h1>{content.side == "r" ? content.title : ""}</h1>
+          <div>{content.side == "r" ? content.content : ""}</div>
         </div>
         <div className="content__title">
           <h3 className="content__title--h3">I'm </h3>
@@ -95,11 +105,13 @@ export default function Home() {
         </div>
         <div
           className={`content__info--${
-            content.expand && content.side == 'l' ? "with-content" : "without-content"
+            content.expand && content.side == "l"
+              ? "with-content"
+              : "without-content"
           }`}
-          >
-          <h1>{content.side=="l"?content.title:""}</h1>
-          <div>{content.side=="l"?content.content:""}</div>
+        >
+          <h1>{content.side == "l" ? content.title : ""}</h1>
+          <div>{content.side == "l" ? content.content : ""}</div>
         </div>
       </div>
       <div></div>
