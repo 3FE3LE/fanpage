2c8fdc9d85e16d07e898aefb06e46ea83ebd90de
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2021-04-09 10:58:50 -0500

    content responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,28 @@ import Head from "next/head";
 import { useState } from "react";
 
 export default function Home() {
-  const [content, setContent] = useState("about");
+  const initialContent = { title: "", content: "", expand: false, side: 'r' };
+
+  const [content, setContent] = useState(initialContent);
 
   const contents = [
     {
-      title: "",
-      content: "",
+      title: "Frontend Developer",
+      content: "copy&paste",
+      expand: true,
+      side: "l",
+    },
+    {
+      title: "About me",
+      content: "copy&paste",
+      expand: true,
+      side: "r",
+    },
+    {
+      title: "Contact",
+      content: "copy&paste",
+      expand: true,
+      side: "l",
     },
   ];
 
@@ -30,32 +46,60 @@ export default function Home() {
         <title>3FE 3LE</title>
       </Head>
       <img
+        onClick={() => setContent(initialContent)}
         className="content__button content__button--logo"
-        src="/logo.svg"
+        src="/logo.png"
         alt=""
       />
-      <button className="content__button content__button--text content__button--text--about">
-        About
-      </button>
-      <button className="content__button content__button--text content__button--text--contact">
-        Contact
-      </button>
-      <button className="content__button content__button--text content__button--text--developer">
+      <button
+        onClick={() => setContent(contents[0])}
+        className="content__button content__button--text content__button--text--developer"
+      >
         Frontend Developer
       </button>
+      <button
+        onClick={() => setContent(contents[1])}
+        className="content__button content__button--text content__button--text--about"
+      >
+        About
+      </button>
+      <button
+        onClick={() => setContent(contents[2])}
+        className="content__button content__button--text content__button--text--contact"
+      >
+        Contact
+      </button>
       <div className="content">
-        <h3 className="content__h3">I'm </h3>
-        <div className="content__name">
-          <div className="name">
-            <h1 className="">
-              3FE <span>{name}</span>
-            </h1>
+        <div
+          className={`content__info--${
+            content.expand && content.side == 'r' ? "with-content" : "without-content"
+          }`}
+        >
+          <h1>{content.side=="r"?content.title:""}</h1>
+          <div>{content.side=="r"?content.content:""}</div>
+        </div>
+        <div className="content__title">
+          <h3 className="content__title--h3">I'm </h3>
+          <div className="content__title--name">
+            <div className="name">
+              <h1 className="">
+                3FE <span>{name}</span>
+              </h1>
+            </div>
+            <div className="name">
+              <h1 className="">
+                3LE <span>{lastName}</span>
+              </h1>
+            </div>
           </div>
-          <div className="name">
-            <h1 className="">
-              3LE <span>{lastName}</span>
-            </h1>
-          </div>
+        </div>
+        <div
+          className={`content__info--${
+            content.expand && content.side == 'l' ? "with-content" : "without-content"
+          }`}
+          >
+          <h1>{content.side=="l"?content.title:""}</h1>
+          <div>{content.side=="l"?content.content:""}</div>
         </div>
       </div>
       <div></div>
